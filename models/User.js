e819cac089831f39da4c8a7dea01737f8c1bc717
Your Name <you@example.com>
2026-08-03 13:27:36 +0200

    password2.0

--- models/User.js
+++ models/User.js
@@ -1,29 +1,4 @@
-// const mongoose = require('mongoose');
 
-// const userSchema = new mongoose.Schema({
-//   username: { 
-//     type: String, 
-//     required: true, 
-//     unique: true, 
-//     trim: true 
-//   },
-//   password: { 
-//     type: String, 
-//     required: true 
-//   },
-//   role: { 
-//     type: String, 
-//     enum: ['admin', 'sales_rep'], 
-//     default: 'sales_rep' 
-//   },
-//   name: String,
-//   createdAt: { 
-//     type: Date, 
-//     default: Date.now 
-//   }
-// });
-
-// module.exports = mongoose.model('User', userSchema);
 
 
 const mongoose = require('mongoose');
